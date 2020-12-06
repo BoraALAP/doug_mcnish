0a27508c246843c9cfa533a9be234060366db2d7
BoraALAP <bora.alap@artticfox.com>
2020-12-06 12:58:19 -0500

    feat: :sparkles: product external links
    Hard coded external links for product type === book

--- src/templates/ProductPage.js
+++ src/templates/ProductPage.js
@@ -12,9 +12,120 @@ import Button from '../components/UI/Button'
 
 const ProductPage = ({ data }) => {
   const product = data.shopifyProduct
+  const info = [
+    {
+      handle: 'classics-veganized',
+      body: [
+        {
+          country: 'Canada',
+          buttons: [
+            {
+              company: 'Amazon',
+              link:
+                'https://www.amazon.ca/gp/product/0735237069/ref=as_li_tl?ie=UTF8&camp=15121&creative=330641&creativeASIN=0735237069&linkCode=as2&tag=dougmcnish-20&linkId=60e5efb3776b43105b83e3d4ba75734a'
+            },
+            {
+              company: 'Chapters Indigo',
+              link:
+                'https://www.chapters.indigo.ca/en-ca/books/product/9780735237063-item.html?s_campaign=aff-001-5056941-Penguin+Random+House+Canada+Limited-PLA-books-00-09-13710563-8727424&cjevent=5c2672f29adb11ea802f07b50a24060b'
+            }
+          ]
+        },
+        {
+          country: 'USA',
+          buttons: [
+            {
+              company: 'Amazon',
+              link:
+                'https://www.amazon.com/gp/product/0735237069?tag=randohouseinc7986-20#ace-6796040015'
+            },
+            {
+              company: 'Barnes and Noble',
+              link:
+                'https://www.barnesandnoble.com/w/the-classics-veganized-doug-mcnish/1136201449;jsessionid=D42EF3C0C9AB07D7D5929C4BA2503DBF.prodny_store01-atgap14?ean=9780735237063&st=AFF&2sid=Random%20House%20Inc_8373827_NA&sourceId=AFFRandom%20House%20Inc'
+            },
+            {
+              company: 'Books a Million (BAM)',
+              link:
+                'https://www.booksamillion.com/p/9780735237063?AID=10747236&PID=8373827&SID=PRHEFFDF5A7F1--9780735237063&cjevent=8318bf789adb11ea8035089b0a240611#'
+            }
+          ]
+        },
+        {
+          country: 'Eastern Europe',
+          buttons: [
+            {
+              company: 'Amazon',
+              link:
+                'https://www.amazon.de/Classics-Veganized-Favourite-Comfort-Lifestyle-ebook/dp/B084787TB9/ref=sr_1_1?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=the+classics+veganized&qid=1598632675&sr=8-1'
+            }
+          ]
+        },
+        {
+          country: 'Western Europe and the UK',
+          buttons: [
+            {
+              company: 'Amazon',
+              link:
+                'https://www.amazon.co.uk/Classics-Veganized-Favourite-Comfort-Lifestyle-ebook/dp/B084787TB9/ref=sr_1_1?dchild=1&keywords=the+classics+veganized&qid=1592841073&sr=8-1'
+            }
+          ]
+        },
+        {
+          country: 'Australia',
+          buttons: [
+            {
+              company: 'Amazon',
+              link:
+                'https://www.amazon.com.au/gp/offer-listing/0735237069/ref=tmm_pap_new_olp_0?ie=UTF8&condition=new&qid=1597260208&sr=8-1'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      handle: 'eat-raw-eat-well-400-raw-vegan-and-gluten-free-recipes',
+      body: `3`
+    },
+    {
+      handle: 'raw-quick-and-delicious-5-ingredient-recipes-in-just-15-minutes',
+      body: `4`
+    },
+    {
+      handle: 'vegan-everyday-500-delicious-recipes',
+      body: `5`
+    }
+  ]
+
+  const item = info.find(
+    it => product.productType === 'Book' && it.handle === product.handle
+  )
+
+  console.log(item)
+
+  console.log(product)
   const thisEdge = data.allServices.edges.find(
     edge => edge.node.id === product.id
   )
+
+  const Form = () => {
+    if (product.productType === 'Book') {
+      return item.body.map(country => (
+        <PurchaseBox>
+          <h3>{country.country}</h3>
+          <Buttons>
+            {country.buttons.map(button => (
+              <a href={button.link}>
+                <Button>{button.company}</Button>
+              </a>
+            ))}
+          </Buttons>
+        </PurchaseBox>
+      ))
+    } else {
+      return <ProductForm product={product} />
+    }
+  }
 
   return (
     <Layout title={product.title || false}>
@@ -26,7 +137,7 @@ const ProductPage = ({ data }) => {
             </Left>
             <Right>
               {product.title && <h1 itemProp="title">{product.title}</h1>}
-              <ProductForm product={product} />
+              <Form />
             </Right>
           </Top>
 
@@ -76,6 +187,24 @@ const Container = styled.div`
   padding: ${({ theme }) => theme.pagePadding};
   grid-gap: 2rem;
 `
+
+const PurchaseBox = styled.div`
+  display: grid;
+  grid-gap: 16px;
+`
+
+const Buttons = styled.div`
+  display: grid;
+  grid-auto-flow: row;
+  grid-gap: 8px;
+
+  @media screen and (min-width: 991px) {
+    grid-auto-flow: column;
+    justify-content: start;
+  }
+`
+
+const H4 = styled.h4``
 
 const Top = styled.div`
   display: grid;
